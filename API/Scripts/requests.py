3cc18ba4936da74c88df262d3914c566e0304906
Acidicts <111660544+Acidicts@users.noreply.github.com>
2024-07-29 11:43:03 +0100

    Pt 2

--- API/Scripts/requests.py
+++ API/Scripts/requests.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, Blueprint, request, redirect
 from random import randint
+from llama_cpp import Llama
 
 requests = Blueprint('api', __name__)
 ntl = {
@@ -20,27 +21,59 @@ ntl = {
 def api():
     inp = request.args.get('code')
 
-    if request.method == 'GET':
-        return jsonify({'message': 'Hello World!'})
-    else:
-        switch = {
-            'help': 'Sending Help!',
-            'hello': "h12",
-            'bye': 'Goodbye!'
-        }
-        oup = switch.get(inp, 'Invalid Code!')
+    switch = {
+        'help': 'Sending Help!',
+        'hello': "h12",
+        'bye': 'Goodbye!'
+    }
+    oup = switch.get(inp, 'Invalid Code!')
 
-        if oup == 'h12':
-            for i in range(randint(1, 50)):
-                temp = str(randint(123456789, 987654321))
-            new = []
-            for length in range(len(temp)):
-                new.append(ntl[int(temp[length])])
+    temp = 0
 
-            temp = ''
-            for l in new:
-                temp = str(temp + l)
+    if oup == 'h12':
+        for i in range(randint(1, 50)):
+            temp = str(randint(123456789, 987654321))
+        new = []
+        for length in range(len(temp)):
+            new.append(ntl[int(temp[length])])
 
-            return temp
+        temp = ''
+        for l in new:
+            temp = str(temp + l)
 
-        return jsonify({'message': str(oup)})
+        return temp
+
+    return jsonify({'message': str(oup)})
+
+
+@requests.route('/')
+def index():
+    return redirect('/api')
+
+
+@requests.route('/help')
+def help():
+    return redirect('/api?code=help')
+
+
+@requests.route('/hello')
+def hello():
+    return redirect('/api?code=hello')
+
+
+@requests.route('/bye')
+def bye():
+    return redirect('/api?code=bye')
+
+
+@requests.route('/ai')
+def ai():
+    model_path = ".. /API/llama-2-7b-chat.Q2_K.gguf"
+
+    prompt = request.args.get('prompt')
+    llm = Llama(model_path=model_path, n_ctx=2048)
+
+    # Generate a response
+    output = llm(prompt)
+
+    return output["choices"][0]["text"]
